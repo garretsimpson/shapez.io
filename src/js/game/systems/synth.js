dbb0a6bbff43c8742cfe3de2dbf334ac845029c9
FatCatX <garretsimpson@yahoo.com>
2021-12-10 18:59:31 -0800

    Update synth.js
    - Added ShapezSynth class.
    - Creates a synth for every drawn display.
    - Covert shapes to tones and plays them on a synth.

--- src/js/game/systems/synth.js
+++ src/js/game/systems/synth.js
@@ -8,35 +8,103 @@ export class SynthSystem extends GameSystem {
     constructor(root) {
         super(root);
 
-        this.displays = "";
-
-        this.synth = new Tone.Synth().toDestination();
+        this.synths = {};
     }
 
-    play() {
-        const root = this.root;
-        const synth = this.synth;
-        const entities = root.systemMgr.systems.display.getDrawnEntities();
+    /**
+     * Get UID and value from each display
+     */
+    getDisplayValues() {
+        let result = {};
+        const entities = this.root.systemMgr.systems.display.getDrawnEntities();
 
-        let displays = "";
         for (let entity of entities) {
-            let value = "none";
+            let shape = "";
             const pinsComp = entity.components.WiredPins;
             const network = pinsComp.slots[0].linkedNetwork;
             if (network && network.hasValue()) {
-                const currentValue = network.currentValue;
-                if (currentValue.getItemType() === "shape") {
-                    value = currentValue.getAsCopyableKey();
+                const value = network.currentValue;
+                if (value.getItemType() === "shape") {
+                    shape = value.getAsCopyableKey();
                 }
             }
+            result[entity.uid] = shape;
+        }
 
-            const display = entity.uid + ": " + value + " ";
-            displays += display;
+        return result;
+    }
+
+    play() {
+        const synths = this.synths;
+        const displayInfo = this.getDisplayValues();
+
+        // Create and delete synths as needed
+        for (let uid of Object.keys(synths)) {
+            if (displayInfo[uid] == undefined) {
+                logger.debug("Delete synth:", uid);
+                synths[uid].dispose();
+                delete synths[uid];
+            }
         }
-        if (this.displays != displays) {
-            this.displays = displays;
-            logger.debug(displays);
-            synth.triggerAttackRelease("C4", "8n");
+        for (let uid of Object.keys(displayInfo)) {
+            if (synths[uid] == undefined) {
+                logger.debug("Create synth:", uid);
+                synths[uid] = new ShapezSynth(uid);
+            }
         }
+
+        // Update synth values
+        for (let uid of Object.keys(synths)) {
+            assert(
+                synths[uid] != undefined && displayInfo[uid] != undefined,
+                "synths not in sync with displays"
+            );
+            synths[uid].update(displayInfo[uid]);
+        }
+    }
+}
+
+class ShapezSynth {
+    constructor(uid) {
+        this.uid = uid;
+        this.synth = new Tone.Synth().toDestination();
+        this.shape = "";
+    }
+
+    /**
+     * Parse the shape and return note value
+     * @param {String} shape
+     * @returns {String}
+     */
+    getNoteFromShape(shape) {
+        const OCTIVE_STR = "CRSW";
+        const TONE_STR = "rygcbpw";
+        const ADJ_STR = "CRS";
+        const TONES = "CDEFGAB";
+        const ADJS = ["b", "", "#"];
+
+        if (!shape) return;
+        if (shape.length < 4) return;
+        const octiveIndex = OCTIVE_STR.indexOf(shape[0]);
+        const toneIndex = TONE_STR.indexOf(shape[1]);
+        const adjIndex = ADJ_STR.indexOf(shape[2]);
+        const note = TONES[toneIndex] + ADJS[adjIndex] + (octiveIndex + 3);
+
+        return note;
+    }
+
+    update(value) {
+        if (this.shape == value) return;
+
+        this.shape = value;
+        this.synth.triggerRelease();
+        const note = this.getNoteFromShape(this.shape);
+        logger.debug(note);
+        if (note == "") return; // release
+        this.synth.triggerAttack(note);
+    }
+
+    dispose() {
+        this.synth.dispose();
     }
 }
